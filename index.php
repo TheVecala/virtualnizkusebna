5346a7bb508871e51aa9d388d1f4834c446cd803
TheV <the@vecala.cz>
2019-08-07 14:52:54 +0200

    console.log ze souboru

--- index.php
+++ index.php
@@ -841,16 +841,11 @@ define ("ROWS", 10);
 </div> 
    
   
- 
+<?php  
+  require "php/console.php";
+?>
   
-    <script>
 
-console.log("DIY forever!");
-console.log("  <?php echo "loged =".$loged.",  "."login= ".$login.", "."kapela= ".$kapela.", "."slozka_souboru= ".$slozka_souboru.",   "."slozka_slozek= ".$slozka_slozek.",  "; ?>  ");
-console.log("  <?php echo "vysledek= ".$vysledek.",  "."id= ".$_SESSION['id'].",  "."aktualni_diskuse =".$aktualni_diskuse.",  "."SESSION aktualni_diskuse= ".$_SESSION['diskuse'].",  "."nazev= ".$_SESSION['nazev'].",  ";  ?>  ");  
-console.log("  <?php echo "sekce = ".$sekce ?> ");
-console.log("  <?php echo "session sekce = ".$_SESSION['sekce_k_zobrazeni'] ?> ");
-</script>
   
   <script>   /*  ------------ vecalovo   */
 $(document).ready(function(){
---
+++ php/console.php
@@ -0,0 +1,22 @@
+<script>
+
+console.log("DIY forever!");
+console.log("  <?php echo "loged = ".$loged.",    login = ".$login.",     kapela = ".$kapela.",    session id = ".$_SESSION['id'] ?>");
+
+console.log("  <?php echo "vysledek = ".$vysledek.",   aktualni_diskuse = ".$aktualni_diskuse.",  SESSION aktualni_diskuse= ".$_SESSION['diskuse'].",  nazev= ".$_SESSION['nazev'];?>  "); 
+
+console.log("  <?php echo "sekce = ".$sekce.",    slozka_slozek= ".$slozka_slozek .",   slozka_souboru = ".$slozka_souboru  ?> ");
+
+console.log("  <?php echo "session sekce_k_zobrazeni = ".$_SESSION['sekce_k_zobrazeni'].",     SESSION chyba_prihlaseni= ". $_SESSION['chyba_prihlaseni'] ?> ");
+</script>
+  
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+ 
