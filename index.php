9d4a4872cfce39842a4c370a2451f1e52004dbab
Unknown <the@vecala.cz>
2019-08-19 23:00:44 +0200

    poskok

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if(isset($_SESSION['playlist']))
 
 if(isset($_SESSION['befelemepesseveze']))
 	   {   $befelemepesseveze =$_SESSION['befelemepesseveze'];
-    } else { $befelemepesseveze=  "72410024" ; } ; 	
+    } else { $befelemepesseveze=  "befelemepesseveze" ; } ; 	
 	
  		 
  ?> 
