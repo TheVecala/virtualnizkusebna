ee885d6b77b7a0c8a16cbe813b19c2ec5f920856
TheV <the@vecala.cz>
2019-11-21 21:08:16 +0100

    posun

--- php/presunout_soubor.php
+++ php/presunout_soubor.php
@@ -1,10 +1,18 @@
 <?php session_start(); ?>
 <?php
  // $target_dir = ($_POST["soubor_ke_smazani"]);
-$soubor_k_presunuti =  "../" .($_POST["soubor_k_presunuti"]);
-$cil_souboru =  "../" .($_POST["cil_souboru"]);
+ 
+  $soubor_k_presunuti =  "../" .($_POST["soubor_k_presunuti"]);
+// $cil_souboru =  "../" .($_POST["cil_souboru"]);
+
+
+ //$soubor_k_presunuti =  "../user/silentroom/338786519/uploads/jamming/jamming_refrenova_smycka_clear.mp3" ;
+$cil_souboru =         "../user/silentroom/338786519/uploads/2_5_1/jamming_ra_clear_ren.mp3";
+
 
 $adresa_pro_navrat =    ($_POST["navrat"]);
+  
+  	rename($soubor_k_presunuti,$cil_souboru);;
   
 if (file_exists($soubor_k_presunuti)) {
     
@@ -12,7 +20,7 @@ if (file_exists($soubor_k_presunuti)) {
 	
 	// unlink($target_file);
 	// rename odkud kam
-	rename($soubor_k_presunuti,$cil_souboru);
+	// rename($soubor_k_presunuti,$cil_souboru);
 	
 }
  
