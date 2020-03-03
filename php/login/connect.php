44bc400a096a57a4666db3d9373f1baa38888ebd
TheV <the@vecala.cz>
2020-03-03 13:10:07 +0100

    priprava na alpha releas

--- php/login/connect.php
+++ php/login/connect.php
@@ -1,8 +1,8 @@
 <?php
 $db_server    = 'localhost'; /* Název serveru, ke kterému se budeme připojovat */
-$db_login     = 'hanakdusan'; /* Jméno uživatele do DB */
-$db_password  = 'serepes6'; /* Heslo uživatele do DB */
-$db_name      = '18810_virtualni_zkusebna'; /* Název databáze, ve které jsme si vytvořili tabulku "uzivatele" */
+$db_login     = 'xxxxxxxxx'; /* Jméno uživatele do DB */
+$db_password  = 'xxxxxxxxxxx'; /* Heslo uživatele do DB */
+$db_name      = 'xxxxxxxxxxxxx'; /* Název databáze, ve které jsme si vytvořili tabulku "uzivatele" */
 $spojeni      = @MySQL_Connect($db_server ,$db_login, $db_password);
 @MySQL_Select_DB($db_name)or die('<p style="color: red">Nastala chyba v pripojeni k databazi');
 mysql_query("set names utf8");
