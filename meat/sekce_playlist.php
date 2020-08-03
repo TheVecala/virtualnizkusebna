afdc734948f0e952cfd11cd55a381e7b89d48f0d
TheV <the@vecala.cz>
2020-08-03 15:37:47 +0200

    oprava stylování vysouvacího playlistu

--- meat/sekce_playlist.php
+++ meat/sekce_playlist.php
@@ -1,4 +1,4 @@
-			<div class=" sticky card bg-dark text-white"  style="margin-top: 1px;"> <!--hlavička prvního sloupce     -->
+			<div class=" sticky  card bg-dark text-white"  style="margin-top: 1px; z-index:4"> <!--hlavička prvního sloupce     -->
                <div class="card-body"> 
 	
 	             <h2 style="text-align:left; color:black ; display: inline ;background-color: #dc3545;">  PLAYLIST </h2> 
