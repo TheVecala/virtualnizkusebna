7e79e669bad72912497466606aa4e836426798ef
TheV <the@vecala.cz>
2020-09-25 15:38:13 +0200

    remove rounded div around logos

--- index.php
+++ index.php
@@ -188,8 +188,7 @@ td  {
 
 .h2, h2 {
  font-size:1.2rem;
- border-radius: 1rem;
- padding: 5px;
+  
  margin: 5px;
 }
 
--- meat/sekce_playlist.php
+++ meat/sekce_playlist.php
@@ -1,7 +1,7 @@
 			<div class=" sticky  card bg-dark text-white"  style="margin-top: 1px; "> <!--hlavička prvního sloupce     -->
                <div class="card-body"> 
 	
-	             <h2 style="text-align:left; display: inline; color:#ffc107 ; background-color:#343a40; font-weight: bold; text-shadow: 2px -2px 20px #ffc107;">  PLAYLIST </h2> 
+	             <h2 style="text-align:left; display: inline; color:#ffc107 ;   font-weight: bold; text-shadow: 2px -2px 20px #ffc107;">  PLAYLIST </h2> 
 				  
 			     <div style="text-align:right; display: inline"   >		                
 				    <button  id="nova_slozka"   type="button" class="btn btn-sm btn-secondary"  style=" display: inline" data-toggle="modal" data-target="#modal_nova_slozka"> NOVÁ SKLADBA </button>
