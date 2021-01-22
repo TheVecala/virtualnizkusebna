8df863f9ca85fb29820db062f22df08c6f3a088b
TheV <the@vecala.cz>
2021-01-22 14:09:58 +0100

    cleaning

---
+++ session_list.php
@@ -0,0 +1,35 @@
+  <?php   
+$_SESSION['prihlasen'] ano nebo ne
+
+$_SESSION['login']
+$_SESSION['kapela'] - 
+$_SESSION['id']
+$_SESSION['nazev'] 
+$_SESSION['cely_nazev']
+
+$_SESSION['befelemepesseveze']
+$_SESSION['sekce_k_zobrazeni']
+$_SESSION['slozka_souboru_k_zobrazeni']
+$_SESSION['diskuse']
+$_SESSION['aktualni_text']
+
+$_SESSION['vysledek']
+
+$_SESSION['chyba_prihlaseni']
+
+$_SESSION['skin']
+
+$_SESSION['playlist']   -   nepoužívané
+
+
+
+
+$_SESSION['']
+$_SESSION['']
+$_SESSION['']
+$_SESSION['']
+$_SESSION['']
+$_SESSION['']
+$_SESSION['']
+
+?>
