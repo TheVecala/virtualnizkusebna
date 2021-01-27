2631638e57e92656367b11a82b95308137e4f93f
TheV <the@vecala.cz>
2021-01-27 15:45:15 +0100

    localizace

--- php/login/connect.php
+++ php/login/connect.php
@@ -2,7 +2,7 @@
 $db_server    = 'localhost'; /* Název serveru, ke kterému se budeme připojovat */
 $db_login     = 'xxxxxxxxx'; /* Jméno uživatele do DB */
 $db_password  = 'xxxxxxxxxxx'; /* Heslo uživatele do DB */
-$db_name      = 'xxxxxxxxxxxxx'; /* Název databáze, ve které jsme si vytvořili tabulku "uzivatele" */
+$db_name      = '18810_zkusebna_multi'; /* Název databáze, ve které jsme si vytvořili tabulku "uzivatele" */
 $spojeni      = @MySQL_Connect($db_server ,$db_login, $db_password);
 @MySQL_Select_DB($db_name)or die('<p style="color: red">Nastala chyba v pripojeni k databazi');
 mysql_query("set names utf8");
