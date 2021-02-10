584f3b2c7b340fc055bca74e3ada14f94f831f02
TheV <the@vecala.cz>
2021-02-10 23:38:24 +0100

    vytvoření nového uživatele funguje

--- php/vytvorit_noveho_uzivatele.php
+++ php/vytvorit_noveho_uzivatele.php
@@ -3,25 +3,19 @@
  session_start();
  include "login/connect.php";
  
-   
-      $new_login= $_POST["login"];
-      $new_md5_heslo= $_POST["heslo"];
+   $new_login= $_POST["login"];   
+   $new_heslo= $_POST["heslo"];  
+   $new_md5_heslo= md5($new_heslo);;  
    $new_nazev_kapely= "silentroom";
-      $new_email= $_POST["email"];
+   $new_email= $_POST["email"];
    $new_hashedkapela= "338786519";
    $new_adresa_diskuse= "diskuse_silentroom_123456789";
-  
-  
-  
-   
-     
   
    
   // ověření neexistence stejného uživatele - viz vytvoření zkušebny 
 	
  
 // vložení do databaze 	
-// $vysledek=mysql_query("insert into $aktualni_diskuse (cas, vzkaz, jmeno) values (".time().",'". $komentar."','".$_POST["name"]."')");
  
    $vysledek=mysql_query("INSERT INTO uzivatele_multi VALUES ('','$new_login','$new_md5_heslo','$new_nazev_kapely','$new_email','$new_hashedkapela','$new_adresa_diskuse')");
    
