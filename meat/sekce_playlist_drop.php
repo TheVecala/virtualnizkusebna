8cd15b78be5a514b3eeb1d604de79963c72ef691
TheV <the@vecala.cz>
2021-09-02 23:41:47 +0200

    design ě

---
+++ meat/sekce_playlist_drop.php
@@ -0,0 +1,141 @@
+			<div class=" sticky  card bg-dark text-white"  style="margin-top: 1px; "> <!--hlavička prvního sloupce     -->
+               <div class="card-body"> 
+	
+	    	
+				 
+	           </div>
+ 		   
+            </div> <!--     -->
+	      		  
+            <div> <!--  playlist   -->
+					  
+			  <div  id=" "  style="background-color:#27a243; padding: 3px;"> <!-- zmšna složky    -->
+			 
+			  	<div>
+					 
+					<div>
+				      <ul class="list-group list-group-flush">
+						<?php 
+						for($x = 0; $x < $delka_pole_slozek; $x++) {
+							  if ($pole_slozek[$x] == ".")  { continue; };
+							  if ($pole_slozek[$x] == "..")  { continue; };
+						   $soub = ($slozka_slozek.$pole_slozek[$x]);
+						  
+						   $label_soub = ($pole_slozek[$x]);  
+						   
+
+						   
+							if(is_dir($soub))
+								  {    
+						    $soubor_s_nazvem = $soub."/data/nazev_valu.txt";
+	 
+	                            if (file_exists($soubor_s_nazvem))  { 
+	 
+								$cely_nazev = fopen($soubor_s_nazvem, "r") ;
+							 } 
+
+ 					 
+							  
+								?>
+						<li class="list-group-item list-group-item-action <?php  if ($pole_slozek[$x]==$slozka_souboru)  { echo"  active " ; } ; ?> " style="padding:0px; <?php  if ($pole_slozek[$x]==$slozka_souboru)  { echo"  background-color:#27a243 ; color: white " ; } ; ?> ">
+
+						<!-- tlačítko změny složky    --> 
+											 
+                            <div style=" " > 
+								  
+									<form action="/php/zmenit_slozku.php" method="post" enctype="multipart/form-data"> 
+									 <input id="navrat" type="text" value="<?php echo $_SERVER['PHP_SELF']; ?>" name="navrat" style="display:none" >
+									 <input id="cilova_slozka" type="text" value="<?php echo $pole_slozek[$x] ?>" name="cilova_slozka" style="display:none" >
+									
+                                <div class="media border p-1" style="font-size: 0.8rem; padding:1px;    ">
+								
+																	   
+									<?php   
+   									   if ($pole_slozek[$x]==$slozka_souboru)  {  ?>   
+								   							   								   	
+									 <img src="/data/icons8-music-record-50.png" alt="složka" style="max-height:30px" class="mr-3  " > 	
+																		
+									 <?php  
+  									 }
+									 
+									 else
+									  {
+										  ?>
+								       
+									  <?php 
+									 	 }
+										
+									  ?>
+								  				
+								   <div class="media-body" style="padding: 0.2rem; max-width: 339px; text-align: right; max-height:100%" >
+																
+									   <div  style="display: inline; position: absolute;  left: 5px;  margin-right:10px;  "> 
+									      <div  style=" text-align: left;  "> 
+									      <?php
+									     	while(!feof($cely_nazev)) {
+											    echo fgets($cely_nazev);
+											    }
+										     	fclose($cely_nazev);
+									      ?> 
+										  </div> 
+									   </div> 
+								       <div  style="  display: inline; position: relative;  z-index: 5; " >
+ 
+								  				
+								    <?php  if ($pole_slozek[$x]==$slozka_souboru)  { }
+									 else
+									  { ?>
+								         <button id=" " type="submit" style=" max-width: 120px ; display: inline; padding: 1px;" class="btn btn-sm btn-warning btn-VZ" value="  <?php echo $pole_slozek[$x] ;?> " name="submit">
+                                          OTEVŘÍT
+									     </button>
+									   </div> 
+									  <?php 
+									 	 }
+									 ; ?>
+	
+									 </form>							   
+	   
+						            </div >	<!-- media body    -->							
+                               	</div>	<!--   media  -->																	
+						    </div>                                      
+ 								   
+						</li> 
+
+								<?php
+								}
+						}
+						?>
+		                </ul>
+					  
+				    </div>
+   <button  id="nova_slozka"   type="button" class="btn btn-sm btn-secondary"  style=" display: inline" data-toggle="modal" data-target="#modal_nova_slozka"> NOVÁ SKLADBA </button>
+				</div>  <!-- playlist_vysuvka   --> 
+
+              </div> 
+			  
+			  
+      <?php if ($delka_pole_slozek < 3	) 
+		{ ?>
+	   	
+	      <div class="card bg-success text-white" style="margin-bottom: 3px;" > 
+							   
+	                          <div class="card-header" style= "  "> 
+							     <p>zatim tu nic není   </p> 
+							  </div>  						   
+							   
+							  <div id="" class="card-body   " style="  "   > 
+							   
+						          <img src="/data/singer.png" alt="-" style= " max-width:100% " > 
+							   
+		   					  </div> <!--card body  -->
+							   
+		  </div> <!-- card   -->	
+		   	
+	 	<?php
+	 	  } 
+	    ?>			  
+			  
+			  
+			  
+			  
+            </div> 
--- meat/sekce_souboru.php
+++ meat/sekce_souboru.php
@@ -8,13 +8,7 @@
 			   
 		               <h2 style="text-align:left; display: inline; color:#ffc107 ; background-color:#343a40; font-weight: bold; text-shadow: 2px -2px 20px #ffc107;">  NAHRÁVKY </h2>
 					   
-  				       <div  style="text-align:right; display: inline">	<!--  tlačítka souboru  -->   	       
-				          <button  id="vlozit_soubor"   type="button" class="btn btn-sm  btn-secondary"  style=" display: inline" data-toggle="modal" data-target="#modal_vlozit_soubor" >VLOŽIT</button>  
-						  
-                          <button  id="nahrat_zvuk"   type="button" class="btn btn-sm  btn-danger"  style=" display: inline" data-toggle="modal" data-target="#modal_nahrat_zvuk" >REC</button> 
-		 
-					  
-			           </div>
+
 	
 	               </div>	
  				
@@ -105,7 +99,16 @@
 					}
 					?>
  		 			
-			</div> <!--  konec vypis souboru   --> 
+	
+			
+			  		  <div  style="text-align:right;"  class=" bg-success text-white" >	<!--  tlačítka souboru  -->   	       
+				          <button  id="vlozit_soubor"   type="button" class="btn btn-sm  btn-secondary"  style=" display: inline" data-toggle="modal" data-target="#modal_vlozit_soubor" >VLOŽIT</button>  
+						  
+                          <button  id="nahrat_zvuk"   type="button" class="btn btn-sm  btn-danger"  style=" display: inline" data-toggle="modal" data-target="#modal_nahrat_zvuk" >REC</button> 
+		 
+					  
+			           </div>		
+				</div> <!--  konec vypis souboru   --> 	
 		  </div>	 <!--  konec vysuvka   --> 
 		  
       <?php if ($delka_pole_souboru < 5	) 
@@ -128,3 +131,4 @@
 	 	<?php
 	 	  } 
 	    ?>
+		
