0ed5faf0623fd2478e11ad8284d1d92515fc7675
TheV <the@vecala.cz>
2022-09-02 22:26:04 +0200

    z playlistu do tracku návrh nové hello.php

--- php/zmenit_slozku.php
+++ php/zmenit_slozku.php
@@ -1,7 +1,12 @@
 <?php session_start();  ?>
 <?php
  $_SESSION['slozka_souboru_k_zobrazeni'] = ($_POST["cilova_slozka"]);
-$adresa_pro_navrat = ($_POST["navrat"]);  
+ 
+ 
+// $adresa_pro_navrat = ($_POST["navrat"]);  
+   $adresa_pro_navrat = "/index.php";  
+
+
 $_SESSION['vysledek'] = "zmenena slozka"; 
 
  
